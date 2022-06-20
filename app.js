e8c6311592ec4ff8c6c44b200e90f240962a229a
ldomino <domino.laurie@gmail.com>
2022-06-19 20:37:57 -0500

    Added security features - limiting how many times api can be called, using secure http headers, preventing mongo query manipulation and also query string manipulation

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,19 +14,57 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
 // MiddleWare - data from the body can be added. req.body is available
 //  with this
 // Only use logging in development mode
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+/ Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
 // No longer necessary.
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware 👋');
 //   next();
 // });
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution - clears up the query string from unwanted parameters.
+// whitelist is an array where we allow duplicates.
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- controllers/authController.js
+++ controllers/authController.js
@@ -19,8 +19,10 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
+    // browser cannot modify it.
     httpOnly: true,
   };
+  // cookie only sent on a secure connection https so production.
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
